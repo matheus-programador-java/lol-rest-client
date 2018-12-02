2446d3f607de90d2a03c6345f1514bd1d2e86a4d
Matheus Rocha Fernandes <matheusrfjava@gmail.com>
2018-12-02 10:10:13 -0200

    modified:   services.js

--- lol-services/services.js
+++ lol-services/services.js
@@ -4,7 +4,7 @@ const findSummoner = axios.create({
     //PASSAR SUMMONER NAME  P/ GET
     baseURL: 'https://br1.api.riotgames.com/lol/summoner/v4/summoners/by-name/',
     params: {
-        api_key: "RGAPI-72bacf56-6829-4ccb-874a-0c3814fd38f1"
+        api_key: "YOU API KEY - SEE https://developer.riotgames.com/ FOR MORE INFORMATION"
     },
 
     transformResponse: [function (data) {
@@ -18,7 +18,7 @@ const findMatch = axios.create({
     //PASSAR SUMMONER ID  P/ GET
     baseURL: 'https://br1.api.riotgames.com/lol/spectator/v4/active-games/by-summoner/',
     params: {
-        api_key: "RGAPI-72bacf56-6829-4ccb-874a-0c3814fd38f1"
+        api_key: "YOU API KEY - SEE https://developer.riotgames.com/ FOR MORE INFORMATION"
     },
 
     transformResponse: [function (data) {
